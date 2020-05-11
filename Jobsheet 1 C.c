b93f46032adb87563fdfc83b103767b6c622bd65
Destiarani <64551347+Destiarani@users.noreply.github.com>
2020-05-11 11:07:21 +0000

    Create Jobsheet 1 C.c
    Membuat array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("Destiarani - F1B019038 \n");
+	printf("Kelompok 8\n\n");
+	char*
+	nim_array[8]={"5","6","8","9","10","23","22","21"};
+	printf("nim ke-1 = %s\n",nim_array[0]);
+	printf("nim ke-2 = %s\n",nim_array[1]);
+	printf("nim ke-3 = %s\n",nim_array[2]);
+	printf("nim ke-4 = %s\n",nim_array[3]);
+	printf("nim ke-5 = %s\n",nim_array[4]);
