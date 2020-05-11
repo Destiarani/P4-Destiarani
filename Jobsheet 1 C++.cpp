012478ab92172c273612f7a417e72e85f5d6fb19
Destiarani <64551347+Destiarani@users.noreply.github.com>
2020-05-11 11:24:21 +0000

    Create Jobsheet 1 C++.cpp
    Membuat Array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"Destiarani-F1B019038\n";
+	cout<<"Kelompok 8\n\n";
+	int nim[8]= {5,6,8,9,10,23,22,21};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+	cout << "Nim ke-8 adalah " << nim[7] << endl;
+	
+}}
